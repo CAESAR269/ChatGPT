be13937a79d1bd88dd12e79c86e1b9e442c9d17a
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-05-30 15:43:09 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
         {
             steps{
             git branch: main,
-            url: https://github.com/CAESAR269/ChatGPT.git
+            url: 'https://github.com/CAESAR269/ChatGPT.git'
         }
     }
        stage('build')
