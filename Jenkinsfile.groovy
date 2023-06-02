5dd19a05bb896d8a04e0f202c8366896c4a6c93a
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-06-02 23:54:08 +0530

    Update and rename Jenkinsfile to Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,15 +2,13 @@
 pipeline {
     agent any
     stages {
-        stage('vcs')
-        {
+        stage('vcs') {
             steps {
                 git branch: main,
                 url: 'https://github.com/CAESAR269/ChatGPT.git'
             }
         }
-        stage('build')
-        {
+        stage('build') {
             steps {
                 sh 'docker image build -t caesar269/chatgpt:latest .'
             }
